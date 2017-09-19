4d5cda2a86e9488be726e6e0bfdd9f7a68b2b39c
litinsky <alitinsky@gmail.com>
2017-09-19 18:46:28 +0300

    sbertest pseudo working version

--- sbertest.py
+++ sbertest.py
@@ -11,17 +11,21 @@ from deeppavlov.agents.paraphraser.paraphraser import EnsembleParaphraserAgent
 import config
 
 
-def run_test(url, tasks_number):
-    print('Number of tasks: %i' % tasks_number)
+#def run_test(url, tasks_number):
+def run_test(param_dict):
+    #print('Number of tasks: %i' % tasks_number)
+    print('Number of tasks: %i' % param_dict['test_tasks_number'])
 
     # Settings
-    data_dir = './build/'
+    #data_dir = './build/'
     rep_models_url = os.environ.get('MODELS_URL')
 
     # Get tasks
     print('Getting tasks...')
-    get_params = {'stage': 'test', 'quantity': tasks_number}
-    get_response = requests.get(url, params=get_params)
+    #get_params = {'stage': 'test', 'quantity': tasks_number}
+    #get_response = requests.get(url, params=get_params)
+    get_params = {'stage': 'test', 'quantity': param_dict['test_tasks_number']}
+    get_response = requests.get(param_dict['rest_url'], params=get_params)
     tasks = json.loads(get_response.text)
     session_id = tasks['id']
 
@@ -38,8 +42,14 @@ def run_test(url, tasks_number):
     # Process observations via algorithm
     print('Processing observations via algorithm...')
     opt = {
-        'fasttext_model': os.path.join(data_dir, 'ft_0.8.3_nltk_yalen_sg_300.bin'),
-        'model_files': [os.path.join(data_dir, 'paraphraser_%i' % i) for i in range(5)],
+        'fasttext_model': os.path.join(param_dict['embeddings_dir'], 'ft_0.8.3_nltk_yalen_sg_300.bin'),
+        #paraphraser/paraphraser_models_final
+        #'model_files': [os.path.join(param_dict['models_dir'], \
+        'model_files': [os.path.join((param_dict['models_dir'] + 'paraphraser/paraphraser_models_final/'), \
+                                  param_dict['model_name'] + '_' + str(i)) \
+                        for i in range(param_dict['test_tasks_number'])],
+        #'fasttext_model': os.path.join(data_dir, 'ft_0.8.3_nltk_yalen_sg_300.bin'),
+        #'model_files': [os.path.join(data_dir, 'paraphraser_%i' % i) for i in range(5)],
         'datatype': 'test'
     }
     agent = EnsembleParaphraserAgent(opt)
@@ -78,7 +88,7 @@ def run_test(url, tasks_number):
     # Post answers data
     print('Posting answers data...')
     post_headers = {'Accept': '*/*'}
-    rest_response = requests.post(url, json=post_dict, headers=post_headers)
+    rest_response = requests.post(param_dict['rest_url'], json=post_dict, headers=post_headers)
     print('Matching result: %s' % rest_response.text)
 
 
@@ -114,19 +124,32 @@ def urljoin(*args):
 
 
 def main():
-    # Initialise test params
-    data_dir = config.DATA_DIR
-    common_models_dir = config.COMMON_MODELS_DIR
+    # Initialise params
+    #data_dir = config.DATA_DIR
+    embeddings_dir = config.EMBEDDINGS_DIR
 
     # Initialise update model params
     update_model_param_dict = {}
     update_model_param_dict['models_dir'] = config.MODELS_DIR
-    update_model_param_dict['repo_url'] = config.MODELS_DIR
+    update_model_param_dict['repo_url'] = config.REPO_URL
     update_model_param_dict['repo_model_url'] = config.REPO_MODEL_URL
+    update_models(update_model_param_dict)
 
+    # Initialise test params
+    test_param_dict = {}
+    test_param_dict['embeddings_dir'] = config.EMBEDDINGS_DIR
+    test_param_dict['embeddings_filename'] = config.EMBEDDINGS_FILENAME
+    test_param_dict['models_dir'] = config.MODELS_DIR
+    test_param_dict['model_name'] = config.MODEL_NAME
+    test_param_dict['ensemble_models_num'] = config.ENSEMBLE_MODELS_NUM
+    test_param_dict['rest_url'] = config.REST_URL
+    test_param_dict['test_tasks_number'] = config.TEST_TASKS_NUMBER
 
     # Execute test
-    run_test('http://api.aibotbench.com/kpi2/qas', 5)
+    #run_test('http://api.aibotbench.com/kpi2/qas', 5)
+    run_test(test_param_dict)
 
 if __name__ == '__main__':
     main()
+
+
