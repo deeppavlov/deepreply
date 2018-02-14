9de639f83eb5902b990680e4377984faa56c808f
alitinsky <alitinsky@gmail.com>
2018-02-14 19:19:47 +0300

    added kpi4en

--- api.py
+++ api.py
@@ -32,10 +32,10 @@ def score():
     """
     kpi_name = request.args.get('kpi_name')
     if kpi_name not in ["kpi1", 'kpi2', "kpi3", "kpi4", "kpi4ru",
-                        "kpi3_2", "kpi3en", "intents"]:
+                        "kpi3_2", "kpi3en", "intents", "kpi4en"]:
         return jsonify({
             'error': 'kpi_name must be one of: kpi1, kpi2, kpi3, kpi4, '
-                     'kpi4ru, kpi3_2, kpi3en, intents'
+                     'kpi4ru, kpi3_2, kpi3en, intents, kpi4en'
         }), 400
 
     tasks_number = request.args.get('tasks_number')
@@ -136,6 +136,20 @@ def answer_kpi4():
        type: json
     """
     return answer("kpi4")
+
+
+@app.route('/answer/kpi4en', methods=['POST'])
+def answer_kpi4en():
+    """
+    KPI 4: SQUAD (English)
+    ---
+    parameters:
+     - name: data
+       in: body
+       required: true
+       type: json
+    """
+    return answer("kpi4en")
 
 
 @app.route('/answer/kpi4ru', methods=['POST'])
